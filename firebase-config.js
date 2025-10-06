45d49cfe17b6b90421a77a7f8827c2fda0461b1e
Proyecto00723 <gustavotellov27@gmail.com>
2025-10-06 00:16:06 -0600

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,17 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDSpeFHjt5Mqytow2Ct_yYJ-4YiRL4ydPo",
+  authDomain: "mi-tienda-7a97a.firebaseapp.com",
+  projectId: "mi-tienda-7a97a",
+  storageBucket: "mi-tienda-7a97a.firebasestorage.app",
+  messagingSenderId: "380330280917",
+  appId: "1:380330280917:web:c0d248b5bde09997fa9f5c"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
