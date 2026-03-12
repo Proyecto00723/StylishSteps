f554736dbcb2b069e0d7fb29429f8bf26215d542
Xtremo Developer <gustavotellov27@gmail.com>
2026-03-12 15:02:30 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,14 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCDSrE_hWvYYgjNwTX85leeU3vHBLBJsY",
-  authDomain: "stylishsteps-10fc4.firebaseapp.com",
-  databaseURL: "https://stylishsteps-10fc4-default-rtdb.firebaseio.com",
-  projectId: "stylishsteps-10fc4",
-  storageBucket: "stylishsteps-10fc4.firebasestorage.app",
-  messagingSenderId: "32645603346",
-  appId: "1:32645603346:web:ae022c525a85aaf7ad38cb",
-  measurementId: "G-369VZP753G"
+  apiKey: "AIzaSyAejrbK9T57bdc5MpYRzvioWdWUKyBOR4Q",
+  authDomain: "ventas-tecnologicas-fe784.firebaseapp.com",
+  databaseURL: "https://ventas-tecnologicas-fe784-default-rtdb.firebaseio.com",
+  projectId: "ventas-tecnologicas-fe784",
+  storageBucket: "ventas-tecnologicas-fe784.firebasestorage.app",
+  messagingSenderId: "673551715331",
+  appId: "1:673551715331:web:830ec279ac978ccf711236"
 };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
